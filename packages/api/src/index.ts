e993b6dab65ee2e8fc9041f992a5d13367a35601
Nathan Skelley <nathan.skelley@bath.edu>
2020-12-07 13:33:13 +0000

    Updated API response again

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -4,7 +4,7 @@ const port = 4000; // default port to listen
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
-    res.send( "Hello World!" );
+    res.send( "Hello from SkelleyBelly!" );
 } );
 
 // start the Express server
