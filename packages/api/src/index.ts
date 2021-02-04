913b45d069d54bb543e1adb837821341b3b9e309
Nathan Skelley <nathan.skelley@bath.edu>
2021-02-04 08:14:51 +0000

    Added global linting

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,13 +1,15 @@
-import express from 'express';
+import express from "express";
+
 const app = express();
 const port = 4000; // default port to listen
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello World!" );
-} );
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 // start the Express server
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
-} );
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`server started at http://localhost:${port}`);
+});
